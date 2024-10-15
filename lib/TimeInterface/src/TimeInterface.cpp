108b3205ae71de020e59c48e96b5f24ed9ba06eb
Sam Groveman <samgroveman@outlook.com>
2024-10-14 21:47:21 -0400

    Added time interface

---
+++ lib/TimeInterface/src/TimeInterface.cpp
@@ -0,0 +1,56 @@
+#include "TimeInterface.h"
+
+// Initialize static variables
+ESP32Time TimeInterface::rtc;
+
+/// @brief Get the current formatted time
+/// @param format The CPP formatted time (https://cplusplus.com/reference/ctime/strftime/)
+/// @return The fomratted time as a string
+String TimeInterface::getFormattedTime(String format) {
+	return rtc.getTime(format);
+}
+
+/// @brief Gets the current date time
+/// @param longDate True to use the long date time
+/// @return The date time as a string
+String TimeInterface::getDateTime(bool longDate) {
+	return rtc.getDateTime(longDate);
+}
+
+/// @brief Sets the time using the passed paramters
+/// @param sc seconds (0-59)
+/// @param mn minute (0-29)
+/// @param hr hour of day (0-23)
+/// @param dy day of month (1-31)
+/// @param mt month (1-12)
+/// @param yr year (for digit)
+/// @param ms microseconds (optional)
+void TimeInterface::setTime(int sc, int mn, int hr, int dy, int mt, int yr, int ms) {
+	rtc.setTime(sc, mn, hr, dy, mt, yr, ms);
+}
+
+
+/// @brief Sets the time usinng time since Unix epoch
+/// @param epoch Epoch time in seconds
+/// @param ms microseconds (optional)
+void TimeInterface::setTime(unsigned long epoch, int ms) {
+	rtc.setTime(epoch, ms);
+}
+
+/// @brief Sets the GMT timezone offset
+/// @param offset The offset in seconds (e.g. 3600 for one hour)
+void TimeInterface::setOffset(long offset) {
+	rtc.offset = offset;
+}
+
+/// @brief Gets the current epoch seconds with the current timezone offset
+/// @return The seconds since the Unix epoch
+long TimeInterface::getEpoch() {
+	return rtc.getEpoch();
+}
+
+/// @brief Gets the current epoch in seconds without timezone offset
+/// @return The seconds since the Unix epoch
+long TimeInterface::getLocalEpoch() {
+	return rtc.getLocalEpoch();
+}
---
+++ lib/TimeInterface/src/TimeInterface.h
@@ -0,0 +1,29 @@
+/*
+* This file and associated .cpp file are licensed under the GPLv3 License Copyright (c) 2024 Sam Groveman
+* 
+* ESP32Time: https://github.com/fbiego/ESP32Time
+*
+* Contributors: Sam Groveman
+*/
+
+#pragma once
+#include <Arduino.h>
+#include <LogBroadcaster.h>
+#include <ESP32Time.h>
+
+/// @brief Provides a static interface to access the current time
+class TimeInterface {
+	public:
+		static String getFormattedTime(String format);
+		static String getDateTime(bool longDate = false);
+		static void setTime(int sc, int mn, int hr, int dy, int mt, int yr, int ms = 0);
+		static void setTime(unsigned long epoch, int ms = 0);
+		static void setOffset(long offset);
+		static long getEpoch();
+		static long getLocalEpoch();
+
+	private:
+		/// @brief Time object to use
+		static ESP32Time rtc;
+
+};
